64366988090a7086060a7866f9be2cde1d91fcfd
Roberto Agredo <memoriaderam@gmail.com>
2024-12-26 12:17:18 -0400

    Update app.js
    Actualizacion de app archivo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  let numeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J','Q','K'];
+  let pools = ['<i class="fas fa-heart" style="font-size:70px;color:red;"></i>', '<img src="diamante.jpg" alt="Trulli" width="70" height="70">', '&#9824', '&#9827'];
+
+  let numerosaleatorios = Math.floor(Math.random() * numeros.length);
+  let poolaleatorio = Math.floor(Math.random()*pools.length);
+
+  let pool_aleatorio = pools[poolaleatorio];
+  let numero_aleatorio = numeros[numerosaleatorios];
+
+  console.log(pool_aleatorio);
+
+  document.querySelector("#numero").innerHTML = numero_aleatorio;
+  document.querySelector("#pool").innerHTML = pool_aleatorio;
+  document.querySelector("#pool2").innerHTML = pool_aleatorio;
+  
+}
+
+
+
